39bfcbfde3175cf29700a43856be51903bf3166b
Douglas Imler <Douglas.Imler@gmail.com>
2024-07-18 17:44:36 -0500

    Hw turn in

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,6 +1,9 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
+    try {lab.runLab(0)} catch {console.log('Data format incorrect: Aborting grading');
+      return null;
+    };
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
@@ -20,5 +23,28 @@ let studentLabs = [
     }
   }
 ];
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
 
 gradeLabs(studentLabs);
