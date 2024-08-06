54bbf05f251848868233b7fafa5026efec964ade
Douglas Imler <Douglas.Imler@gmail.com>
2024-08-06 01:59:19 -0500

    yup

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,7 +3,23 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
+    button.addEventListener('click', function(){
+        paragraph.innerHTML = 'Houston we have liftoff!'
+    })
+
+    missionAbort.addEventListener('mouseover', function(){
+        missionAbort.style.backgroundColor = 'red';
+    })
+
+    missionAbort.addEventListener('mouseout', function(){
+        missionAbort.style.backgroundColor = "";
+    })
     // Put your code for the exercises here.
+    missionAbort.addEventListener('click', function(){
+        if (confirm('Do you want to abort?')){
+            paragraph.innerHTML = 'Mission Aborted!'
+        }
+    })
     
 }
 
